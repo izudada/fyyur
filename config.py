c9d326824df1f2d6d920780aca022ed14e3386e4
Anthony <tonyudeagbala@gmail.com>
2022-05-31 20:10:00 +0100

    Use Heroku db

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = 'postgres://kglxeccpqyvjym:db4c80e5f915ef03dfc6a09e96fb1ce8b790d0d714c6ff69aedccf95cb723171@ec2-3-228-235-79.compute-1.amazonaws.com:5432/df9k8ltv4qqp4l'
 
 # Alter Postgresql dialect name in heroku
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
