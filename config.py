df5a263dbf41cfcd199641a22e38464edee5eb0b
Anthony <tonyudeagbala@gmail.com>
2022-05-31 20:26:11 +0100

    Fix db security

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+from decouple import config
+
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -10,8 +13,7 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://kglxeccpqyvjym:db4c80e5f915ef03dfc6a09e96fb1ce8b790d0d714c6ff69aedccf95cb723171@ec2-3-228-235-79.compute-1.amazonaws.com:5432/df9k8ltv4qqp4l'
-
+SQLALCHEMY_DATABASE_URI = config('DATABASE_URL')
 # Alter Postgresql dialect name in heroku
 if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
     SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
